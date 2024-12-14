c265165d9a6901301c7f882e88811aba16281943
Eba Khalil <ebaqadomi7@gmail.com>
2024-12-14 20:13:23 +0200

    Initial commit

---
+++ home_db.sql
@@ -0,0 +1,184 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 13, 2023 at 06:10 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `home_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `name`, `password`) VALUES
+('BcjKNX58e4x7bIqIvxG7', 'admin', '6216f8a75fd5bb3d5f22b6f9958cdede3fc086c2'),
+('LTrRmmartMUleiepQr1p', 'ali', '8cb2237d0679ca88db6464eac60da96345513964');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `number` varchar(10) NOT NULL,
+  `message` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `name`, `email`, `number`, `message`) VALUES
+('PLkon1pWCa5vF2tbg0Hv', 'samah', 'sheeha-samah@hotmail.com', '0598766545', 'can you help me');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `property`
+--
+
+CREATE TABLE `property` (
+  `id` varchar(20) NOT NULL,
+  `user_id` varchar(20) NOT NULL,
+  `property_name` varchar(50) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `price` varchar(10) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `offer` varchar(10) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `furnished` varchar(50) NOT NULL,
+  `bhk` varchar(10) NOT NULL,
+  `deposite` varchar(10) NOT NULL,
+  `bedroom` varchar(10) NOT NULL,
+  `bathroom` varchar(10) NOT NULL,
+  `balcony` varchar(10) NOT NULL,
+  `carpet` varchar(10) NOT NULL,
+  `age` varchar(2) NOT NULL,
+  `total_floors` varchar(2) NOT NULL,
+  `room_floor` varchar(2) NOT NULL,
+  `loan` varchar(50) NOT NULL,
+  `lift` varchar(3) NOT NULL DEFAULT 'no',
+  `security_guard` varchar(3) NOT NULL DEFAULT 'no',
+  `play_ground` varchar(3) NOT NULL DEFAULT 'no',
+  `garden` varchar(3) NOT NULL DEFAULT 'no',
+  `water_supply` varchar(3) NOT NULL DEFAULT 'no',
+  `power_backup` varchar(3) NOT NULL DEFAULT 'no',
+  `parking_area` varchar(3) NOT NULL DEFAULT 'no',
+  `gym` varchar(3) NOT NULL DEFAULT 'no',
+  `shopping_mall` varchar(3) NOT NULL DEFAULT 'no',
+  `hospital` varchar(3) NOT NULL DEFAULT 'no',
+  `school` varchar(3) NOT NULL DEFAULT 'no',
+  `market_area` varchar(3) NOT NULL DEFAULT 'no',
+  `image_01` varchar(50) NOT NULL,
+  `image_02` varchar(50) NOT NULL,
+  `image_03` varchar(50) NOT NULL,
+  `image_04` varchar(50) NOT NULL,
+  `image_05` varchar(50) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `property`
+--
+
+INSERT INTO `property` (`id`, `user_id`, `property_name`, `address`, `price`, `type`, `offer`, `status`, `furnished`, `bhk`, `deposite`, `bedroom`, `bathroom`, `balcony`, `carpet`, `age`, `total_floors`, `room_floor`, `loan`, `lift`, `security_guard`, `play_ground`, `garden`, `water_supply`, `power_backup`, `parking_area`, `gym`, `shopping_mall`, `hospital`, `school`, `market_area`, `image_01`, `image_02`, `image_03`, `image_04`, `image_05`, `description`, `date`) VALUES
+('Nu3VfI692jFF1pyccbZ3', 'v1lUHhgm6zZSJDqqu0EG', 'home', 'nablus,palestine ', '500', 'flat', 'sale', 'ready to move', 'furnished', '1', '4', '3', '2', '1', '300', '4', '16', '3', 'available', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes', 'no', 'no', 'no', 'no', 'no', 'eOMeE6TP7g85oOOmUHwh.jpg', '2RHd2PyfeHTa26tDAtow.png', 'WxuReYucAyrtwNG4mqEG.jpg', '', '', '3 bedroon ,2 bathroom,garden', '2023-05-13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requests`
+--
+
+CREATE TABLE `requests` (
+  `id` varchar(20) NOT NULL,
+  `property_id` varchar(20) NOT NULL,
+  `sender` varchar(20) NOT NULL,
+  `receiver` varchar(20) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `requests`
+--
+
+INSERT INTO `requests` (`id`, `property_id`, `sender`, `receiver`, `date`) VALUES
+('bFPX0DCJuUaGmti5wGK3', 'Nu3VfI692jFF1pyccbZ3', 'v1lUHhgm6zZSJDqqu0EG', 'v1lUHhgm6zZSJDqqu0EG', '2023-05-13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `saved`
+--
+
+CREATE TABLE `saved` (
+  `id` varchar(20) NOT NULL,
+  `property_id` varchar(20) NOT NULL,
+  `user_id` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `saved`
+--
+
+INSERT INTO `saved` (`id`, `property_id`, `user_id`) VALUES
+('QEf3OThbShYWvIyYU4Qz', 'Nu3VfI692jFF1pyccbZ3', 'v1lUHhgm6zZSJDqqu0EG');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `number` varchar(10) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `number`, `email`, `password`) VALUES
+('dHW0uc61JOVjch0xR9CX', 'samah', '0598766545', 'samah@gmail.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220'),
+('v1lUHhgm6zZSJDqqu0EG', 'saly', '0598765432', 'saly@gmail.com', 'f56d6351aa71cff0debea014d13525e42036187a');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ project/project/about.php
@@ -0,0 +1,214 @@
+<?php  
+
+include 'components/connect.php';
+
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}else{
+   $user_id = '';
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>About Us</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<!-- about section starts  -->
+
+<section class="about">
+
+   <div class="row">
+      <div class="image">
+         <img src="images/about-img.svg" alt="">
+      </div>
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>The company itself is a very successful company. They provide for these pains and pleasures, the distinction between the hardships of the truth of life and the present flees from where?</p>
+         <a href="contact.html" class="inline-btn">contact us</a>
+      </div>
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- steps section starts  -->
+
+<section class="steps">
+
+   <h1 class="heading">3 simple steps</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/step-1.png" alt="">
+         <h3>search property</h3>
+         <p>The company itself is a very successful company. Great, please.</p>
+      </div>
+
+      <div class="box">
+         <img src="images/step-2.png" alt="">
+         <h3>contact agents</h3>
+         <p>The company itself is a very successful company. Great, please.</p>
+      </div>
+
+      <div class="box">
+         <img src="images/step-3.png" alt="">
+         <h3>enjoy property</h3>
+         <p>The company itself is a very successful company. Great, please.</p>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- steps section ends -->
+
+<!-- review section starts  -->
+
+<section class="reviews">
+
+   <h1 class="heading">client's reviews</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="user">
+            <img src="images/pic-1.png" alt="">
+            <div>
+               <h3>ali ahmad</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+         <p>Thank you very much. I got the pleasures of choosing the distinction that follow the error that takes him at the time of finding out!</p>
+      </div>
+
+      <div class="box">
+         <div class="user">
+            <img src="images/pic-2.png" alt="">
+            <div>
+               <h3>maryam deo</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+         <p>Thank you very much. I got the pleasures of choosing the distinction that follow the error that takes him at the time of finding out!</p>
+      </div>
+
+      <div class="box">
+         <div class="user">
+            <img src="images/pic-3.png" alt="">
+            <div>
+               <h3>khalil sami</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+         <p>Thank you very much. I got the pleasures of choosing the distinction that follow the error that takes him at the time of finding out!</p>
+      </div>
+
+      <div class="box">
+         <div class="user">
+            <img src="images/pic-4.png" alt="">
+            <div>
+                <h3>yusra ahmad</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+         <p>Thank you very much. I got the pleasures of choosing the distinction that follow the error that takes him at the time of finding out!</p>
+      </div>
+
+      <div class="box">
+         <div class="user">
+            <img src="images/pic-5.png" alt="">
+            <div>
+               <h3>mohamad ali</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+         <p>Thank you very much. I got the pleasures of choosing the distinction that follow the error that takes him at the time of finding out!</p>
+      </div>
+
+      <div class="box">
+         <div class="user">
+            <img src="images/pic-6.png" alt="">
+            <div>
+               <h3>sara absi</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+         <p>Thank you very much. I got the pleasures of choosing the distinction that follow the error that takes him at the time of finding out!</p>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- review section ends -->
+
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
